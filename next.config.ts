46c758783ada1ffc2fb4cd98761fbe685cb8630b
Tito Olubakinde <tolubaki@udel.edu>
2025-01-28 00:10:17 -0500

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config: { externals: { "utf-8-validate": string; bufferutil: string; canvas: string; }[]; }) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-      canvas: "commonjs canvas",
-    });
-    // config.infrastructureLogging = { debug: /PackFileCache/ };
-    return config;
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
+    remotePatterns:   [
       {
         protocol: "https",
-        hostname: "liveblocks.io",
-        port: "",
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+        hostname: 'liveblocks.io',
+        post: ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
